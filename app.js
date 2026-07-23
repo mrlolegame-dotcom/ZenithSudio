19a406696ba240dd15841e1fd84447d45d9e940a
mrlolegame-dotcom <mrlolegame@gmail.com>
2026-07-23 14:17:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,122 @@
+let cart = 0;
+
+
+
+function addCart(name){
+
+cart++;
+
+document.getElementById("cartNumber").innerHTML = cart;
+
+
+alert(
+"✅ "+name+" ajouté au panier"
+);
+
+
+clickEffect(event);
+
+}
+
+
+
+
+function searchProduct(){
+
+let text =
+document
+.getElementById("search")
+.value
+.toLowerCase();
+
+
+
+document
+.querySelectorAll(".product")
+.forEach(product=>{
+
+
+let name =
+product.dataset.name;
+
+
+if(name.includes(text)){
+
+product.style.display="block";
+
+}else{
+
+product.style.display="none";
+
+}
+
+
+});
+
+
+}
+
+
+
+
+
+function filter(type){
+
+
+document
+.querySelectorAll(".product")
+.forEach(product=>{
+
+
+if(
+type=="all" ||
+product.dataset.type==type
+){
+
+product.style.display="block";
+
+}else{
+
+product.style.display="none";
+
+}
+
+
+});
+
+
+}
+
+
+
+
+
 function clickEffect(e){
+
 
 let z=document.createElement("div");
 
+
 z.innerHTML="Z";
 
+
 z.style.position="fixed";
+
 z.style.left=e.clientX+"px";
+
 z.style.top=e.clientY+"px";
 
 z.style.fontSize="50px";
+
 z.style.color="#8b5cf6";
+
 z.style.fontWeight="900";
 
-z.style.animation="jump .8s forwards";
+z.style.animation="jump .8s";
 
 
 document.body.appendChild(z);
 
-
-let sound=new Audio(
-"https://www.soundjay.com/buttons/sounds/button-16.mp3"
-);
-
-sound.volume=0.3;
-sound.play();
 
 
 setTimeout(()=>{
@@ -31,5 +124,6 @@ setTimeout(()=>{
 z.remove();
 
 },800);
+
 
 }
